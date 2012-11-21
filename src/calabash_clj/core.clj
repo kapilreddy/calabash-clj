71ef3d80a7df0277539ce062801ac013a1bc0faa
Kapil Reddy <reddy.kapil@gmail.com>
2012-11-21 23:48:27 +0530

    Fix for previous commit.

--- src/calabash_clj/core.clj
+++ src/calabash_clj/core.clj
@@ -5,7 +5,5 @@
 (defn -main
   "Build an android project and run calabash queries on it."
   [project-path emulators]
-  (android-build/-main project-path emulators (fn []
-                                                ;; Write calabash queries here.
-                                                ;; (android/touch "css" "div")
-                                                )))
+  (android-build/run-on-emulators project-path emulators (fn []
+                                                               (android/command "click_on_view_by_id" "button1"))))
